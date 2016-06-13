422a6a3a82658b827741319c0641f2e6d4a7d82b
Nick Phillips <phillipn101@gmail.com>
2016-06-13 12:57:09 -0500

    Ready for heroku

--- app.rb
+++ app.rb
@@ -38,9 +38,13 @@ class HangpersonApp < Sinatra::Base
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
+    
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    redirect '/show'
+    @game.guess(letter)
+    redirect '/show' if @game.check_win_or_lose == :play
+    redirect '/win' if @game.check_win_or_lose == :win
+    redirect '/lose' if @game.check_win_or_lose == :lose
   end
   
   # Everytime a guess is made, we should eventually end up at this route.
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,10 +8,55 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word, :guesses, :wrong_guesses
+  
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
-
+  
+  def guess(letter)
+    raise ArgumentError.new("Letter cannot be blank") if letter !~ /\S/
+    raise ArgumentError.new("Letter needs to be a letter") if letter !~ /[a-zA-Z]/
+    letter.downcase!
+    
+    if @word.include?(letter)
+      if @guesses.include?(letter) 
+        return false
+      else
+        @guesses << letter
+      end 
+    elsif @wrong_guesses.include?(letter)
+        return false
+    else
+        @wrong_guesses << letter
+    end
+  end
+  
+  def word_with_guesses
+    @revealed = ''
+    
+    @word.split(//).each do |i|
+      if @guesses.include?(i)
+        @revealed += i
+      else
+        @revealed += '-'
+      end
+    end
+    return @revealed
+  end
+  
+  def check_win_or_lose
+    if self.word_with_guesses == @word
+      :win
+    elsif @wrong_guesses.length == 7
+      :lose
+    else
+      :play
+    end
+  end
+  
   def self.get_random_word
     require 'uri'
     require 'net/http'
